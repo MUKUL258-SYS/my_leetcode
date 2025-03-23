4d3649923faeb1b12cf8e5c719ca52cba64a2552
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-03-22 23:23:44 -0700

    Time: 733 ms (42.64%), Space: 14.2 MB (99.67%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
-    vector<string> ans;
-    bool isSafe(int x, int y, vector<vector<char>> &board)
-    {
+    vector<pair<int, int>> movements = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    bool isSafe(int x, int y, vector<vector<char>> &board) {
         int n = board.size(), m = board[0].size();
         return (x >= 0 && y >= 0 && x < n && y < m && board[x][y] != '#');
     }
-    vector<pair<int, int>> movements = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    void func(int x, int y, string &curr, vector<vector<char>> &board, unordered_set<string> &st)
-    {
-        if (curr.size() >= 10)
-            return;
-        if (!isSafe(x, y, board))
-            return;
+
+    void func(int x, int y, string &curr, vector<vector<char>> &board, unordered_set<string> &st, vector<string> &ans) {
+        if (curr.size() >= 10) return;
+        if (!isSafe(x, y, board)) return;
+
         curr += board[x][y];
-        if (st.find(curr) != st.end())
-        {
+        if (st.find(curr) != st.end()) {
             st.erase(curr);
             ans.push_back(curr);
         }
-        curr.pop_back();
+
         char ch = board[x][y];
-        for (auto it : movements)
-        {
-            int nx = x + it.first;
-            int ny = y + it.second;
-            curr += board[x][y];
-            board[x][y] = '#';
-            func(nx, ny, curr, board, st);
-            curr.pop_back();
-            board[x][y] = ch;
+        board[x][y] = '#';  // Mark visited
+
+        for (auto &[dx, dy] : movements) {
+            int nx = x + dx, ny = y + dy;
+            func(nx, ny, curr, board, st, ans);
         }
+
+        board[x][y] = ch;  // Unmark
+        curr.pop_back();    // Correct backtracking
     }
+
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        unordered_set<string> st;
-        for (auto it : words)
-            st.insert(it);
+        unordered_set<string> st(words.begin(), words.end());
+        vector<string> ans;
         int n = board.size(), m = board[0].size();
-        for (int i = 0; i < n; ++i)
-        {
-            for (int j = 0; j < m; ++j)
-            {
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
                 string curr = "";
-                func(i, j, curr, board, st);
+                func(i, j, curr, board, st, ans);
             }
         }
         return ans;
