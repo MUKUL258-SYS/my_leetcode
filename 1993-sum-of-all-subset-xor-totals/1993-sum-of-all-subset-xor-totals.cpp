b719cb0958ee502b506f80d9cccda65ab7d95e56
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-04-05 15:50:16 -0700

    Time: 3 ms (39.94%), Space: 9 MB (90.03%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<(1<<n);i++){
+            int x=0;
+           for(int j=0;j<n;j++){
+            if(i&(1<<j)){
+                x=x^nums[j];
+            }
+           }
+           sum+=x;
+
+        }
+        return sum;
+    }
+};
