14c8f3b521cd5f07f34853f90ebf1d268f8d9f1c
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-04-13 00:16:10 -0700

    Time: 698 ms (18.49%), Space: 25 MB (79.81%) - LeetHub

--- 3808-longest-palindrome-after-substring-concatenation-ii/3808-longest-palindrome-after-substring-concatenation-ii.cpp
+++ 3808-longest-palindrome-after-substring-concatenation-ii/3808-longest-palindrome-after-substring-concatenation-ii.cpp
@@ -1,56 +1,50 @@
 class Solution {
 public:
-    int dp[1001][1001][3];
-    vector<int> sp,tp;
-    bool isPalindrome(const string &s, int l, int r) {
-        while (l < r) {
-            if (s[l++] != s[r--]) 
-                return false;
-        }
-        return true;
+vector<int>sp,tp;
+int dp[1001][1001][3];
+int n,m;
+bool is_palindrome(string &s,int i,int j){
+    while(i<=j){
+        if(s[i++]!=s[j--])return false;
     }
-
-    vector<int> getMaxPalindromes(const string &s) {
-        int n = s.size();
-        vector<int> res(n, 0);
-        for (int i = 0; i < n; ++i) {
-            for (int j = n - 1; j >= i; --j) {
-                if (isPalindrome(s, i, j)) {
-                    res[i] = j - i + 1;
-                    break;
-                }
+    return true;
+}
+vector<int>pre_comp(string &s){
+    int n=s.size();
+    vector<int>res(n,0);
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=0;j--){
+            if(is_palindrome(s,i,j)){
+               res[i]=(j-i+1);
+               break;
             }
         }
-        return res;
     }
-
-    int solve(int i, int j, string &s, string &t, int f) {
-        if (i >= s.size() && j >= t.size()) return 0;
-        if (i >= s.size()) return tp[j];  
-        if (j >= t.size()) return sp[i];
-        if (dp[i][j][f] != -1) return dp[i][j][f];
-        
-        int res = max(sp[i], tp[j]); 
-        
-        if (s[i] == t[j]) {
-                res = max(res, 2 + solve(i + 1, j + 1, s, t, 0));
-            
-        }
-        
-        if (f == 2) {
-            int option1 = solve(i + 1, j, s, t, 2);
-            int option2 = solve(i, j + 1, s, t, 2);
-            res = max(res, max(option1, option2));
-        }
-        
-        return dp[i][j][f] = res;
+    return res;
+}
+int dfs(int i,int j,string &s,string &t,int f){
+    if(i>=n&&j>=m)return 0;
+    if(i>=n)return tp[j];
+    if(j>=m)return sp[i];
+    if(dp[i][j][f]!=-1)return dp[i][j][f];
+    int res=max(sp[i],tp[j]);
+    if(s[i]==t[j]){
+        res=max(res,2+dfs(i+1,j+1,s,t,0));
     }
-
+    if(f==2){
+        res=max(res,dfs(i+1,j,s,t,2));
+        res=max(res,dfs(i,j+1,s,t,2));
+    }
+    return dp[i][j][f]=res;
+}
     int longestPalindrome(string s, string t) {
-        reverse(t.begin(), t.end());
-        sp = getMaxPalindromes(s);
-        tp = getMaxPalindromes(t);
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, s, t, 2);
+        reverse(t.begin(),t.end());
+        n=s.size();
+        m=t.size();
+   sp=pre_comp(s);
+   tp=pre_comp(t);
+   memset(dp,-1,sizeof(dp));
+  return dfs(0,0,s,t,2);
+
     }
 };
