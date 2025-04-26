f1a9d3bc8b5edd890d8a0785c39f6c82c512c45e
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-04-26 02:10:33 -0700

    Time: 27 ms (73.82%), Space: 12 MB (24.71%) - LeetHub

--- 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
+++ 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
 vector<vector<int>>requests;
-int ans;
-void dfs(int i,vector<int>&indegree,int n,int cnt){
-    if(i==requests.size()){
-        for(int i=0;i<n;i++){
-            if(indegree[i]!=0)return ;
-        }
-        ans=max(ans,cnt);
-        return;
-    }
-    indegree[requests[i][0]]--;
-    indegree[requests[i][1]]++;
-    dfs(i+1,indegree,n,cnt+1);
-    indegree[requests[i][0]]++;
-    indegree[requests[i][1]]--;
-    dfs(i+1,indegree,n,cnt);
+int n,m;
 
+int ans;
+void dfs(int i,vector<int>&vp,int cnt){
+    if(i==m){
+          for(int ii=0;ii<n;ii++){
+            if(vp[ii]!=0)return ;
+          }
+       ans=max(ans,cnt);
+       return;
+    }
+    vp[requests[i][0]]--;
+    vp[requests[i][1]]++;
+    dfs(i+1,vp,cnt+1);
+    vp[requests[i][0]]++;
+    vp[requests[i][1]]--;
+    dfs(i+1,vp,cnt);
 }
     int maximumRequests(int n, vector<vector<int>>& requests) {
         this->requests=requests;
-        vector<int>indegree(n,0);
-        dfs(0,indegree,n,0);
+        this->n=n;
+        m=requests.size();
+        vector<int>vp(n,0);
+        ans=0;
+        dfs(0,vp,0);
         return ans;
     }
 };
