76c3ecb056a1187b187c87b6caa9d6465299d9d5
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-04-30 19:48:58 -0700

    Time: 0 ms (100%), Space: 8.9 MB (73.76%) - LeetHub

--- 1065-binary-string-with-substrings-representing-1-to-n/1065-binary-string-with-substrings-representing-1-to-n.cpp
+++ 1065-binary-string-with-substrings-representing-1-to-n/1065-binary-string-with-substrings-representing-1-to-n.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
-string helper(int n){
-    string str="";
-    while(n>0){
-   str+=((n%2)+'0');
-   n/=2;
-    }
-    reverse(str.begin(),str.end());
-    cout<<str<<endl;
-    return str;
-}
-    bool queryString(string s, int n) {
-        int m=s.size();
-        unordered_map<string,int>mp;
-        for(int i=0;i<m;i++){
-            string temp="";
-            for(int j=i;j<m;j++){
-                temp+=s[j];
-                mp[temp]++;
+    bool queryString(string S, int n) {
+        while(n>0){
+            auto s=bitset<32>(n--).to_string();
+            if(S.find(s.substr(s.find("1")))==string::npos)
+                return false;
             }
-        }
-        for(int i=1;i<=n;i++){
-            if(mp[helper(i)]>0)continue;
-            return false;
-            }
-        return true;
+            return true;
     }
 };
