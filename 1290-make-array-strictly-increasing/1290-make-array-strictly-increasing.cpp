a570fa5382642ef5a22c3f895b53ee7af3bb3823
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-05-11 02:20:00 -0700

    Time: 135 ms (82.06%), Space: 82.5 MB (14.99%) - LeetHub

---
+++ 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int n,m;
+int dp[2005][2005][4];
+int dfs(int i,int j,int p,vector<int>&arr1,vector<int>&arr2,int take){
+    if(i>=n)return 0;
+    j=upper_bound(arr2.begin(),arr2.end(),p)-arr2.begin();
+    if(dp[i][j][take]!=-1)return dp[i][j][take];
+    if(j>=m&&arr1[i]<=p)return 2001;
+    int tuke=2001;
+    int notake=2001;
+    if(j<m){
+        tuke=1+dfs(i+1,j+1,arr2[j],arr1,arr2,1);
+    }
+    if(arr1[i]>p){
+        notake=dfs(i+1,j+1,arr1[i],arr1,arr2,0);
+    }
+    return dp[i][j][take]=min(tuke,notake);
+}
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        n=arr1.size();
+        m=arr2.size();
+        memset(dp,-1,sizeof(dp));
+        sort(arr2.begin(),arr2.end());
+        int ans= dfs(0,0,-1,arr1,arr2,0);
+       return ans>=2001?-1:ans;
+    }
+};
