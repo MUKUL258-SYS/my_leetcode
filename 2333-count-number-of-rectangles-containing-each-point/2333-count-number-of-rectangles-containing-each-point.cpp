1c18744668e8b330fead6c9c32287c6e7d1b8c01
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-10-04 01:03:30 -0700

    Time: 220 ms (16.48%), Space: 100.3 MB (16.48%) - LeetHub

--- 2333-count-number-of-rectangles-containing-each-point/2333-count-number-of-rectangles-containing-each-point.cpp
+++ 2333-count-number-of-rectangles-containing-each-point/2333-count-number-of-rectangles-containing-each-point.cpp
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
 public:
     vector<int> countRectangles(vector<vector<int>>& rectangles, vector<vector<int>>& points) {
        unordered_map<int,vector<int>>mp;
@@ -13,11 +13,11 @@ public:
             int x=j[0];
             int y=j[1];
             int cnt=0;
-           for(auto i:mp){
-            vector<int>v=i.second;
-            if(i.first>=y){
+           for(auto &[y2,v]:mp){
+    
+            if(y2>=y){
                auto it=lower_bound(v.begin(),v.end(),x);
-               cnt+=(v.end()-it);
+             if(it!=v.end())cnt+=(v.end()-it);
             }
            }
            vp.push_back(cnt);
@@ -27,43 +27,4 @@ public:
     }
     
     
-};
-*/
-
-class Solution {
-public:
-    vector<int> countRectangles(vector<vector<int>>& rectangles, vector<vector<int>>& points) {
-        sort(rectangles.begin(),rectangles.end());
-        int n=rectangles.size();
-        map<int,vector<int>> he;
-        vector<int> ret;
-        for(int i=0;i<n;i++)
-        {
-            int l=rectangles[i][0];
-            int h=rectangles[i][1];
-            he[h].push_back(l);
-        }
-        int p=points.size();
-        for(int i=0;i<p;i++)
-        {
-            int cnt=0;
-            int l=points[i][0];
-            int h=points[i][1];
-            for(auto& [x,lst]:he)
-            {
-                if(x<h)
-                continue;
-                else
-                {
-                    auto it = lower_bound(lst.begin(),lst.end(),l);
-                    if(it!=lst.end())
-                    {
-                        cnt+=lst.size()-(it-lst.begin());
-                    }
-                }
-            }
-            ret.push_back(cnt);
-        }
-        return ret;
-    }
 };
