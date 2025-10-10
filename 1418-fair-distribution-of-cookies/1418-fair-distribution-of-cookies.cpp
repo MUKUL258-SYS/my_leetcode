fc7a40277efbc958a050d8b1822e20b1bc5f8fdd
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-10-10 00:20:12 -0700

    Time: 708 ms (54.73%), Space: 8.9 MB (71.97%) - LeetHub

---
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int k;
+int ans;
+vector<int>arr;
+void dfs(int i,vector<int>&temp){
+    int n=arr.size();
+     if(i>=n){
+   int maxi=0;
+        for(auto i:temp){
+           maxi=max(maxi,i);
+        }
+        ans=min(ans,maxi);
+        return;
+     }
+
+     for(int j=0;j<k;j++){
+        temp[j]+=arr[i];
+        dfs(i+1,temp);
+        temp[j]-=arr[i];
+     }
+}
+    int distributeCookies(vector<int>& cookies, int k) {
+        this->k=k;
+        arr=cookies;
+        ans=INT_MAX;
+        vector<int>temp(k,0);
+        dfs(0,temp);
+        return ans;
+    }
+};
