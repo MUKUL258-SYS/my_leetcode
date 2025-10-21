6386a7b43a56b4e8d35b7a66711018e1e6051762
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-10-21 03:49:48 -0700

    Time: 32 ms (34.77%), Space: 32 MB (34.41%) - LeetHub

---
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/2293-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+     unordered_map<char,int>mp;
+     for(auto i:s)mp[i]++;
+     for(auto i:t)mp[i]--;
+     int cnt=0;
+     for(auto i:mp)cnt+=abs(i.second);
+     return cnt;   
+    }
+};
