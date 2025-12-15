6b79edcf81a97cf242bcc0fe8281de35b56845d1
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2025-12-14 23:26:58 -0800

    Time: 3 ms (44.18%), Space: 152.9 MB (15.66%) - LeetHub

---
+++ 3644-maximum-k-to-sort-a-permutation/3644-maximum-k-to-sort-a-permutation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sortPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int ans=-1;
+        for(int i=0;i<n;i++){
+         if(i!=nums[i]){
+           // ans=min(ans,(nums[i]&i));
+           ans=(nums[i]&ans);
+         }
+        }
+        return (ans==-1)?0:ans;
+    }
+};
+
