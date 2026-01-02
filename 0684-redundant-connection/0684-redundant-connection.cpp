d9f3fffcaaed01863924007dce6f37e447fd322f
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-01-01 16:43:19 -0800

    Time: 0 ms (100%), Space: 13 MB (64.42%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,36 @@
+class DisjointSet{
+    vector<int>parent,depth;
+    public:
+    DisjointSet(int n):parent(n),depth(n){
+        depth.assign(n,1);
+        iota(parent.begin(),parent.end(),0);
+    }
+    int getRoot(int x){
+        if(x==parent[x])return x;
+        return parent[x]=getRoot(parent[x]);
+    }
+    bool merge(int x,int y){
+        int rootX=getRoot(x);
+        int rootY=getRoot(y);
+        if(rootX==rootY)return false;
+        if(depth[rootX]>depth[rootY])
+            swap(rootX,rootY);
+        parent[rootX]=rootY;
+        if(depth[rootX]==depth[rootY])
+            depth[rootY]++;
+        return true;
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+         DisjointSet ds(n+1);
+         for(auto &edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            if(!ds.merge(u,v))return edge;
+         }
+         return {};
+    }
+};
