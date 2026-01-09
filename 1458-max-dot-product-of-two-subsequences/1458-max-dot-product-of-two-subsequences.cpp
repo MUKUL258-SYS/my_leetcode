379e2402eb1bbbce6cb58ac485e9d7af2a10fee3
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-01-08 21:46:21 -0800

    Time: 47 ms (15.1%), Space: 15.5 MB (74.29%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,39 +1,30 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> nums1, nums2;
-    vector<vector<int>> memo;
-    int n, m;
-    const int NEG_INF = -1e9;
+int n,m;
+int dp[550][550];
+int dfs(int i,int j,vector<int>&nums1,vector<int>&nums2){
+   if(i>=n||j>=m)return 0;
+   if(dp[i][j]!=-1)return dp[i][j]; 
+   int op1=(nums1[i]*nums2[j])+dfs(i+1,j+1,nums1,nums2);
+   int op2=dfs(i+1,j+1,nums1,nums2);
+   int op3=dfs(i+1,j,nums1,nums2);
+   int op4=dfs(i,j+1,nums1,nums2);
+   return dp[i][j]=max({op1,op3,op4});
+}
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        n=nums1.size();
+        m=nums2.size();
+        memset(dp,-1,sizeof(dp));
 
-    int dp(int i, int j) {
-        if (i == n || j == m)
-            return NEG_INF;
+        int ans=dfs(0,0,nums1,nums2);
+        if(ans==0){
+             int v1=*max_element(nums1.begin(),nums1.end());
+             int v2=*max_element(nums2.begin(),nums2.end());
+             int v3=*min_element(nums1.begin(),nums1.end());
+             int v4=*min_element(nums2.begin(),nums2.end());
+             ans=max(v1*v4,v2*v3);
 
-        if (memo[i][j] != INT_MIN)
-            return memo[i][j];
-
-        int take = nums1[i] * nums2[j];
-
-        int res = max({
-            take + dp(i + 1, j + 1), // take both and continue
-            take,                   // take and end here
-            dp(i + 1, j),            // skip nums1[i]
-            dp(i, j + 1)             // skip nums2[j]
-        });
-
-        return memo[i][j] = res;
-    }
-
-    int maxDotProduct(vector<int>& a, vector<int>& b) {
-        nums1 = a;
-        nums2 = b;
-        n = nums1.size();
-        m = nums2.size();
-
-        memo.assign(n, vector<int>(m, INT_MIN));
-        return dp(0, 0);
+        }
+        return ans;
     }
 };
