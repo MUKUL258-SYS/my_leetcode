37feef3524b076995ec88dd23d27a1d9a2611aa0
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-01-08 21:06:14 -0800

    Time: 0 ms (100%), Space: 16 MB (95.87%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,48 +11,22 @@
  */
 class Solution {
 public:
-TreeNode* lca(TreeNode* p,TreeNode* q,TreeNode* root){
-    if(!root)return NULL;
-    if(root==p)return root;
-    if(root==q)return root;
-    TreeNode* l=lca(p,q,root->left);
-    TreeNode* r=lca(p,q,root->right);
-    //if(root==p)return root;
-    //if(root==q)return root;
-    if(l&&r){
-         return root;
-    }
-    if(l)return l;
-    if(r)return r;
-    return NULL;
-
+pair<int,TreeNode*>dfs(TreeNode* root){
+    if(!root)return {0,NULL};
+pair<int,TreeNode*>l=dfs(root->left);
+pair<int,TreeNode*>r=dfs(root->right);
+if(l.first>r.first){
+    return {l.first+1,l.second};
+}
+else if(r.first>l.first){
+    return {r.first+1,r.second};
+}
+else {
+    return {l.first+1,root};
+}
+return{0,NULL};
 }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        queue<TreeNode*>q;
-        unordered_map<int,vector<TreeNode*>>mp;
-        q.push(root);
-        int l=0;
-        while(!q.empty()){
-            int sz=q.size();
-                while(sz--){
-                auto m=q.front();
-               mp[l].push_back(m);
-               q.pop();
-               if(m->left)q.push(m->left);
-               if(m->right)q.push(m->right);
-            }
-            l++;
-        }
-        /*for(auto i:mp){
-            for(auto j:i.second)cout<<j<<" ";
-            cout<<endl;
-        }
-        */
-        vector<TreeNode*>v=mp[l-1];
-        TreeNode* t=v[0];
-        for(int i=1;i<v.size();i++){
-           t=lca(t,v[i],root);
-        }
-        return t;
+        return dfs(root).second;
     }
 };
