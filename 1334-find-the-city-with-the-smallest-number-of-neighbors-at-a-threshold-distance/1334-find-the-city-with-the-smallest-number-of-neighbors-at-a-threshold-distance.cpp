69f11950a70c6f4eb21c1e3ef1f5f8d4cd6a1972
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-01-25 20:22:34 -0800

    Time: 16 ms (46.34%), Space: 17.2 MB (37.08%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,79 +1,38 @@
 class Solution {
 public:
-int d;
-int n;
-vector<vector<pair<int,int>>>adj;
-/*int bfs(int node){
-    queue<pair<int,int>>q;
-    q.push({node,0});
-    vector<int>vis(n,false);
-    vis[node]=true;
-    int cnt=0;
-    while(!q.empty()){
-auto f=q.front();
-int dist=f.second;
-int node=f.first;
-if(dist<=d)cnt++;
-else continue;
-q.pop();
-for(auto i:adj[node]){
-    auto {nei,w}=i;
-    if(!vis[nei]){
-        vis[nei]=true;
-        q.push({nei,dist+w});
-    }
-}
-    }
-    return cnt;
-    
-}
-*/
-int bfs(int node){
-    int cnt=0;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-    pq.push({0,node});
-    vector<int>dist(n,INT_MAX);
-    dist[node]=0;
-    while(!pq.empty()){
-        auto [cd,u]=pq.top();
-        pq.pop();
-       
-        if(cd>dist[u])continue;
-        if(cd>d)break;
-        cnt++;
-        for(auto [v,w]:adj[u]){
-            if(dist[u]+w<dist[v]){
-             dist[v]=w+dist[u];
-             pq.push({dist[v],v});
+    int findTheCity(int n, vector<vector<int>>& edges, int d) {
+        vector<vector<int>>dis(n,vector<int>(n,10005));
+      for(int i=0;i<n;i++){
+        dis[i][i]=0;
+      }
+      for(auto i:edges){
+        dis[i[0]][i[1]]=i[2];
+        dis[i[1]][i[0]]=i[2];
+      }
+      for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j]);
             }
         }
-        
+      }
+      int mini=10005;
+      int ans;
+      for(int i=0;i<n;i++){
+        int cnt=0;
 
-    }
-    return cnt-1;
-}
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        this->n=n;
-   d=distanceThreshold;
-   adj.resize(n);
-   
-   for(auto i:edges){
-     adj[i[0]].push_back({i[1],i[2]});
-     adj[i[1]].push_back({i[0],i[2]});
-   }
-   int mini=INT_MAX;
-   //int maxi=INT_MIN;
-   int ans;
-        for(int i=0;i<n;i++){
-           int v=bfs(i);
-           if(v<mini){
-            mini=v;
-            ans=i;
-           }
-           else if(v==mini){
-             ans=max(ans,i);
-           }
+        for(int j=0;j<n;j++){
+               if(dis[i][j]<=d)cnt++;
         }
-        return ans;
+        if(cnt<mini){
+            mini=cnt;
+            ans=i;
+        }
+        else if(cnt==mini){
+           // mini=min(mini,i);
+           ans=max(ans,i);
+        }
+      }
+      return ans;
     }
 };
