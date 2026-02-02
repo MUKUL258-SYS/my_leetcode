b34a80345444c6a93c06c6a861846b29985d729b
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-02-02 01:59:35 -0800

    Time: 4 ms (32.87%), Space: 14.8 MB (36.49%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -4,17 +4,18 @@ vector<int>res;
 int n;
 void dfs(long long current){
     if(current>n)return ;
-     res.push_back(current);
+    // res.push_back(current);
    for(int i=0;i<=9;i++){
     int temp=current*10+i;
     if(temp>n)break;
-   // res.push_back(temp);
+    res.push_back(temp);
     dfs(temp);
    }
 }
     vector<int> lexicalOrder(int n) {
         this->n=n;
         for(int i=1;i<=min(n,9);i++){
+            res.push_back(i);
                 dfs(i);
         }
         return res;
