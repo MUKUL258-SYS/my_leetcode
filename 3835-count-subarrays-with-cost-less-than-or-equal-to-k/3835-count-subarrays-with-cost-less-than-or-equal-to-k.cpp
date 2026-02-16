430fed064f5854cca048c96f02a6ae51d750332e
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-02-16 02:00:37 -0800

    Time: 549 ms (13.86%), Space: 299.4 MB (31.47%) - LeetHub

---
+++ 3835-count-subarrays-with-cost-less-than-or-equal-to-k/3835-count-subarrays-with-cost-less-than-or-equal-to-k.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        set<pair<long long,int>>s;
+        int n=nums.size();
+            long long ans=0;
+        
+        int i=0;
+        for(int j=0;j<n;j++){
+            s.insert({nums[j],j});
+            //int maxi=(*s.rbegin()).first;
+            //int mini=(*s.begin()).first;
+            while(!s.empty()&&i<=j){
+                if(((*s.rbegin()).first-(*s.begin()).first)*(j-i+1) <=k)break;
+                s.erase({nums[i],i});
+                i++;
+            }
+            int maxi=(*s.rbegin()).first;
+            int mini=(*s.begin()).first;
+            long long cost=1ll*(maxi-mini)*(j-i+1);
+            if(cost<=k){
+                long long len=(j-i+1);
+                 ans+=len;
+            }
+        }
+        return ans;
+
+
+    }
+};
