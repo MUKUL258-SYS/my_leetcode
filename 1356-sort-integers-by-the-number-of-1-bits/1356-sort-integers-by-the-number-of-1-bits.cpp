f1ca46a3ae7c1eeecc1803ae4761ee52df2fe985
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-02-24 18:04:44 -0800

    Time: 4 ms (32.29%), Space: 14.6 MB (39.68%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,15 +1,7 @@
 class Solution {
-    
 public:
     vector<int> sortByBits(vector<int>& arr) {
-      /*  sort(arr.begin(),arr.end(),[&](const int & a,const int &b){
-            int x=__builtin_popcount(a);
-            int y=__builtin_popcount(b);
-            if(x<y)return a<b;
-            else return a<b;
-        });
-        */
-        vector<pair<int,int>>v;
+         vector<pair<int,int>>v;
         for(auto i:arr){
             int x=__builtin_popcount(i);
             v.push_back({x,i});
@@ -18,6 +10,5 @@ public:
         arr.clear();
         for(auto i:v)arr.push_back(i.second);
         return arr;
-
     }
 };
