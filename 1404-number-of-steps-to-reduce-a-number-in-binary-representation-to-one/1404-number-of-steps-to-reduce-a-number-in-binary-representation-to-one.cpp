16fad45102e7020834748130f40d0af072c88f05
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-02-25 22:52:44 -0800

    Time: 0 ms (100%), Space: 8.8 MB (83.16%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-void helper(string &s){
-   /* int carry=1;
-    int i=s.size()-1;
-    string res="";
-    int inc=1;
-    while(i>=0||carry){
-           int sum=(i>=0?s[i]-'0':0)+carry;
-           carry=sum/2;
-           res+=((sum%2)+'0');
-           i--;
-    }
-    return res;
-    */
-    int i=s.size()-1;
-    while(i>=0&&s[i]=='1'){
-        s[i]='0';
-        i--;
-    }
-    if(i>=0)s[i]='1';
-    else s='1'+s;
-    
-}
-
     int numSteps(string s) {
-        int cnt=0;
-        while(s!="1"){
-            if(s.back()=='0'){
-              s.pop_back();
-            }
-            else{
-               helper(s);
-            }
-            cnt+=1;
+        int carry=0;
+       // int i=s.size()-1;
+       int n=s.size();
+        int steps=0;
+       for(int i=n-1;i>0;i--){
+        int digit=s[i]-'0'+carry;
+        if(digit==1){
+             steps+=2;
+             carry=1;
         }
-        return cnt;
+        else steps++;
+       }
+       return steps+carry;
     }
 };
