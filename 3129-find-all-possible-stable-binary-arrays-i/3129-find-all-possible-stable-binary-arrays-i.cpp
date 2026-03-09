6a96391b29cdf8d3829d2b3b5e688c3646280ae7
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-03-09 02:42:22 -0700

    Time: 235 ms (61.79%), Space: 8.7 MB (94.31%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
-    int limit;
-    int dp[201][201][2];
-
-    long long solve(int z, int o, int last) {
-        if (z == 0 && o == 0) return 1;
-        if (dp[z][o][last] != -1) return dp[z][o][last];
-
-        long long ans = 0;
-        if (last == 1) { // Last was 1, must place 0s now
-            for (int i = 1; i <= min(z, limit); ++i) {
-                ans = (ans + solve(z - i, o, 0)) % mod;
-            }
-        } else { // Last was 0, must place 1s now
-            for (int i = 1; i <= min(o, limit); ++i) {
-                ans = (ans + solve(z, o - i, 1)) % mod;
-            }
-        }
-
-        return dp[z][o][last] = ans;
+int limit;
+int mod=1e9+7;
+int dp[201][201][2];
+int dfs(int z,int o,int last){
+    if(o==0&&z==0){
+        return 1;
     }
-
+    if(dp[z][o][last]!=-1)return dp[z][o][last];
+    long long ans=0;
+    if(last==1){
+        for(int i=1;i<=min(z,limit);i++){
+           ans=(ans+dfs(z-i,o,0))%mod;
+        }
+    }
+    else{
+        for(int i=1;i<=min(o,limit);i++){
+            ans=(ans+dfs(z,o-i,1))%mod;
+        }
+    }
+    return dp[z][o][last]=ans;
+}
     int numberOfStableArrays(int zero, int one, int limit) {
-        this->limit = limit;
-        memset(dp, -1, sizeof(dp));
-        // We can start by placing either a block of 0s or a block of 1s
-        // We pass 'last' as the opposite to trigger the initial loops
-        return (solve(zero, one, 0) + solve(zero, one, 1)) % mod;
+        this->limit=limit;
+           memset(dp,-1,sizeof(dp));
+           return (dfs(zero,one,0)+dfs(zero,one,1))%mod;
     }
 };
