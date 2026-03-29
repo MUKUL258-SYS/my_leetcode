a69c752942cda43e80facb76339d49aa070986cd
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-03-28 23:53:29 -0700

    Time: 47 ms (32%), Space: 22.2 MB (32%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        map<char,int>e1,o1,e2,o2;
+        int n=s1.size();
+        for(int i=0;i<n;i+=2){
+            e1[s1[i]]++;
+            e2[s2[i]]++;
+        }
+        for(int i=1;i<n;i+=2){
+            o1[s1[i]]++;
+            o2[s2[i]]++;
+        }
+        return (o1==o2&&e1==e2);
+    }
+};
