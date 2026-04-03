95c7b76e1bccf55adab899d5f45046580cd8c6e0
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-04-02 19:14:59 -0700

    Time: 11 ms (61.94%), Space: 13.4 MB (72.2%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -1,60 +1,29 @@
-#include <vector>
-#include <algorithm>
-#include <cmath>
-
-using namespace std;
-
 class Solution {
-    int memo[12][1 << 12]; // N is at most 12 for this problem
-
-    int dfs(int node, int mask, vector<vector<int>>& graph) {
-        // Base Case: If only one bit is set, the distance is 0
-        if ((mask & (mask - 1)) == 0) {
-            return 0;
-        }
-
-        // Return cached result if already calculated
-        if (memo[node][mask] != -1) {
-            return memo[node][mask];
-        }
-
-        // Initialize with a large value to avoid cycles
-        memo[node][mask] = 1e9;
-
-        for (int neighbor : graph[node]) {
-            if (mask & (1 << neighbor)) {
-                // If neighbor is already in the mask, we check two paths:
-                // 1. Moving to neighbor without changing the mask
-                int visited = 1 + dfs(neighbor, mask, graph);
-                
-                // 2. Moving to neighbor assuming 'node' was the most recently added 
-                //    (backtracking the mask)
-                int notVisited = 1 + dfs(neighbor, mask ^ (1 << node), graph);
-
-                memo[node][mask] = min({memo[node][mask], visited, notVisited});
-            }
-        }
-
-        return memo[node][mask];
-    }
-
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        int n = graph.size();
-        int endingMask = (1 << n) - 1;
-        
-        // Initialize memo table with -1
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= endingMask; j++) {
-                memo[i][j] = -1;
+        int n=graph.size();
+        if(n==1)return 0;
+        int maxi=(1<<n)-1;
+        vector<vector<bool>>vis(n,vector<bool>(1<<n,false));
+        queue<tuple<int,int,int>>q;
+        for(int i=0;i<n;i++){
+            int mask=(1<<i);
+            q.emplace(i,mask,0);
+            vis[i][mask]=true;
+        }
+        while(!q.empty()){
+            auto [node,mask,dist]=q.front();
+            q.pop();
+            if(mask==maxi)return dist;
+            for(auto nei:graph[node]){
+               int newmask=mask|(1<<nei);
+               if(!vis[nei][newmask]){
+                q.emplace(nei,newmask,dist+1);
+                vis[nei][newmask]=true;
+               }
             }
         }
+        return -1;
 
-        int minPath = 1e9;
-        for (int i = 0; i < n; i++) {
-            minPath = min(minPath, dfs(i, endingMask, graph));
-        }
-
-        return minPath;
     }
 };
