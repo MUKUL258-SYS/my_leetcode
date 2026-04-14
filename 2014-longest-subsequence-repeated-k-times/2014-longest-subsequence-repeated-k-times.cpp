b4e88e77a0a6232b09043a001b1c05658e7b9065
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-04-13 17:28:29 -0700

    Time: 150 ms (69.46%), Space: 11.5 MB (56.89%) - LeetHub

--- 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times.cpp
+++ 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times.cpp
@@ -1,85 +1,47 @@
 class Solution {
 public:
-    string result = "";
-
-    bool isSubsequence(string &s, string& sub, int k) {
-        int i = 0;
-        int j = 0;
-        int L = sub.length();
-        int n = s.length();
-
-        while(i < n && j < k*L) {
-            if(s[i] == sub[j%L])
-                j++;
-            
-            i++;
+string res="";
+bool isSub(string &s, string &t, int k){
+      int i=0,j=0;
+    int cnt=0;
+    int n=s.size();
+    int m=t.size();
+    for(int i=0;i<n;i++){
+        if(s[i]==t[j]){
+            j++;
         }
-
-        return j == k * L;
+        if(j==m){
+            cnt++;
+            j=0;
+        }
     }
+    return cnt>=k;
+}
 
-    bool backtracking(string& s, string &curr, vector<bool>& canUse, vector<int>& requiredFreq, int k, int maxLen) {
-        if(curr.length() == maxLen) {
-            if(isSubsequence(s, curr, k)) {
-                result = curr;
-                return true;
-            }
-            return false;
-        }
-
-        for(int i = 25; i >= 0; i--) {
-            if(requiredFreq[i] == 0)
-                continue;
-            
-            //DO
-            char ch = i + 'a';
-            curr.push_back(ch);
-            requiredFreq[i]--;
-
-            //EXPLORE
-            if(backtracking(s, curr, canUse, requiredFreq, k, maxLen) == true) {
-                return true;
-            }
-
-            //UNDO
-            curr.pop_back();
-            requiredFreq[i]++;
-
-        }
-
-        return false;
-    }
 
     string longestSubsequenceRepeatedK(string s, int k) {
-        int n = s.length();
-
-        int freq[26] = {};
-        for(char &ch : s) {
-            freq[ch-'a']++;
+        string str="";
+       vector<int>freq(26,0);
+       for(auto &ch:s){
+        freq[ch-'a']++;
+       }
+       for(int i=0;i<26;i++){
+        if(freq[i]>=k)str+=(i+'a');
+       }
+       queue<string>q;
+       q.push("");
+       string res="";
+       while(!q.empty()){
+       auto temp=q.front();
+       q.pop();
+       for(auto &ch:str){
+        string next=temp+ch;
+        if(isSub(s,next,k)){
+            res=next;
+            q.push(next);
         }
-
-        vector<bool> canUse(26, false);
-        vector<int> requiredFreq(26, 0);
-        for(int i = 0; i < 26; ++i) {
-            if(freq[i] >= k) {
-                canUse[i] = true;
-                requiredFreq[i] = freq[i]/k; //atmost this can be used in a subsequence
-            }
-        }
-
-        int maxLen = n / k;
-
-        for(int len = maxLen; len >= 0; len--) {
-            vector<int> tempRequiredFreq = requiredFreq;
-            string curr = "";
-            if(backtracking(s, curr, canUse, tempRequiredFreq, k, len) == true) {
-                return result;
-            }
-        }
-
-        return result;
-
-
+       }
+       }
+       return res;
     }
 };
-
