c1af11ab7d5beab996e7ff1b4541ede4ce42646d
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-04-17 17:20:16 -0700

    Time: 4 ms (55.15%), Space: 127.6 MB (86.06%) - LeetHub

--- 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
@@ -4,6 +4,7 @@ public:
         int n=nums.size();
         int cnt=0;
         for(int i=0;i<n;i++){
+            if(cnt>nums[i])return false;
             nums[i]-=cnt;
             cnt+=nums[i];
             if(i+1>=k){
