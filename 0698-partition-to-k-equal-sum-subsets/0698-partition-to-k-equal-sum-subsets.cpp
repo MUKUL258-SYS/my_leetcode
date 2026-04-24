e6f257d2bc014261d64562200c8fc534bb21dcfd
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-04-24 13:50:53 -0700

    Time: 23 ms (57.51%), Space: 24.1 MB (15.17%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-int t,n;
+int t;
 vector<int>nums;
-
-int dfs(int k,int curr_sum,int mask,vector<int>&dp){
+int dfs(int k,vector<int>&dp,int mask,int curr){
+    int n=nums.size();
     if(k==0){
-        return mask== (1<<n)-1;
+        return mask+1==(1<<n);
+    }
+    if(curr==t){
+        return dfs(k-1,dp,mask,0);
     }
     if(dp[mask]!=-1)return dp[mask];
-    if(curr_sum==t){
-        return dp[mask]=dfs(k-1,0,mask,dp);
-    }
-    int ans=0;
+int  ans=0;
     for(int i=0;i<n;i++){
-        if(curr_sum+nums[i]<=t){
+      if(curr+nums[i]<=t){
         if((mask&(1<<i))==0){
-          int newmask=mask|(1<<i);
-          ans=ans||dfs(k,curr_sum+nums[i],newmask,dp);
-          newmask^=(1<<i);
-         if(ans){
-            return dp[mask]=ans;
-         }
-     }
-  }
-}
-return dp[mask]=ans;
+            mask|=(1<<i);
+            ans=ans||dfs(k,dp,mask,curr+nums[i]);
+            if(ans){
+                return dp[mask];
+            }
+            mask^=(1<<i);
+
+        }
+      }  
+    }
+    return dp[mask]=ans;
+
 }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        this->nums=nums;
-   long long sum=0;
-    n=nums.size();
-   for(auto i:nums)sum+=i;
-   if(sum%k)return false;
-   t=sum/k;
-   vector<int>dp(1<<n,-1);
-   return dfs(k,0,0,dp);
+       int sum=0;
+       int n=nums.size();
+       this->nums=nums;
+       for(auto i:nums){
+        sum+=i;
+       } 
+       if(sum%k)return false;
+      t=sum/k;
+      vector<int>dp(1<<n,-1);
+    return dfs(k,dp,0,0);
+
     }
 };
