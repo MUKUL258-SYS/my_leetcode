3aafd9a3869acb46b54b568d2640fa3fbebac520
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-04-28 22:00:01 -0700

    Time: 2339 ms (6.28%), Space: 350.4 MB (5.31%) - LeetHub

--- 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -1,8 +1,9 @@
-/*class Solution {
+class Solution {
 public:
 //vector<int>v;
 //unordered_map<string,long long>mp;
-long long dp[2001][4000];
+//long long dp[2001][4000];
+vector<vector<int>>dp;
 long long  dfs(int i,long long sum,vector<int>&v){
     int n=v.size();
     if(i>=n){
@@ -34,32 +35,8 @@ long long  dfs(int i,long long sum,vector<int>&v){
        // for(auto i:s)v.push_back(i);
        set<int> s(vp.begin(), vp.end());
         vector<int> nums(s.begin(), s.end());
-        memset(dp,-1,sizeof(dp));
+       // memset(dp,-1,sizeof(dp));
+       dp.resize(2001,vector<int>(4000,-1));
    return dfs(0,0,nums);
-    }
-};
-*/
-class Solution {
-public:
-    int ans;
-    int solve(int i,int n,int r,vector<int>&v,vector<vector<int>>&dp) {
-        if(i==n) {
-            return r;
-        }
-        if(dp[i][r]!=-1)
-            return dp[i][r];
-        int include=r;
-        if(v[i]>r) {
-            include=solve(i+1,n,r+v[i],v,dp);
-        }
-        int exclude=solve(i+1,n,r,v,dp);
-        return dp[i][r]=max(include,exclude);
-    }
-    int maxTotalReward(vector<int>& v) {
-        sort(v.begin(),v.end());
-        int n=v.size();
-        vector<vector<int>>dp(2001,vector<int>(4001,-1));
-        ans=solve(0,n,0,v,dp);
-        return ans;
     }
 };
