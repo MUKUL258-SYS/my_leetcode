8fb01fe25f4e5fdd7e0d08fe968ea3aa3466ff3c
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-05-03 16:21:42 -0700

    Time: 12 ms (56.48%), Space: 34.6 MB (25.69%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
+        stack<int>s1,s2;
         int n=s.size();
-        if(n&1) return false;
-        int bal=0;
         for(int i=0;i<n;i++){
-            if(s[i]=='('||locked[i]=='0')bal++;
-            else bal--;
+            if(locked[i]=='0'){
+                s2.push(i);
+                continue;
+            }
+            if(s[i]=='('){
+                s1.push(i);
+              continue;
+            }
+            if(s1.size()){
+                s1.pop();
+            }
+            else if(s2.size()){
+                s2.pop();
+            }
+            else return false;
             
-            if(bal<0)return false;
         }
-        bal=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]==')'||locked[i]=='0')bal++;
-            else bal--;
-            
-            if(bal<0)return false;
+        while(s1.size()&&s2.size()){
+            if(s1.top()>s2.top())return false;
+            s1.pop();
+            s2.pop();
         }
-        return true;
+        return s1.empty()&&((s2.size()%2)==0);
     }
 };
