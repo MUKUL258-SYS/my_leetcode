ea9c1487191f7a9617ec4ab40127880784d5865c
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-05-03 21:37:09 -0700

    Time: 20 ms (61.06%), Space: 70.4 MB (79.94%) - LeetHub

--- 1798-maximum-number-of-consecutive-values-you-can-make/1798-maximum-number-of-consecutive-values-you-can-make.cpp
+++ 1798-maximum-number-of-consecutive-values-you-can-make/1798-maximum-number-of-consecutive-values-you-can-make.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int getMaximumConsecutive(vector<int>& coins) {
         sort(coins.begin(),coins.end());
-        int target=coins.back();
-       
-        int sum=0;
         int n=coins.size();
+        int sum=0;
         for(int i=0;i<n;i++){
             if(sum+1>=coins[i]){
-               sum+=coins[i];
+                sum+=coins[i];
             }
             else break;
-             
         }
         return sum+1;
     }
