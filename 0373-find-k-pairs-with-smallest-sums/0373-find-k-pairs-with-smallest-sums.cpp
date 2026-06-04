6831869f9b6e28cda460d8ed4c9c5dacc2d2fef9
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-06-03 22:55:31 -0700

    Time: 161 ms (10.24%), Space: 177 MB (7.08%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-int n=nums1.size();
-int m=nums2.size();
-        vector<vector<int>>res;
+        priority_queue<vector<int>,vector<vector<int>>,greater<>>pq;
+        int n=nums1.size();
+        int m=nums2.size();
         pq.push({nums1[0]+nums2[0],0,0});
-        set<pair<int,int>>vis;
-        vis.insert({0,0});
+        vector<vector<int>>res;
+        //vis[{0,0}]=1;
+        map<pair<int,int>,int>vis;
+         vis[{0,0}]=1;
         while(k--&&pq.size()){
             auto v=pq.top();
+            pq.pop();
             int i=v[1];
             int j=v[2];
-        res.push_back({nums1[i],nums2[j]});
-            pq.pop();
-            if(i+1<n && !vis.count({i+1,j})){
+            res.push_back({nums1[i],nums2[j]});
+            if(i+1<n&&vis[{i+1,j}]==0){
                 pq.push({nums1[i+1]+nums2[j],i+1,j});
-                vis.insert({i+1,j});
+                 vis[{i+1,j}]=1;
             }
-            if(j+1<m && !vis.count({i,j+1})){
+            if(j+1<m&&vis[{i,j+1}]==0){
                 pq.push({nums1[i]+nums2[j+1],i,j+1});
-                vis.insert({i,j+1});
+            vis[{i,j+1}]=1;
             }
-
         }
         return res;
     }
