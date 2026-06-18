f5e177f6016ee5c9d7fe92eea70a7196a1ee22ba
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-06-18 03:07:35 -0700

    Time: 0 ms (100%), Space: 10.4 MB (73.39%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int a=max(ax1,bx1);
-        int b=min(ax2,bx2);
-        int c=min(ay2,by2);
-        int d=max(ay1,by1);
-        int d1=max(0,b-a);
-        int d2=max(0,c-d);
-        int ar2=d1*d2;
-        int ar1=max(0,ax2-ax1)*max(0,ay2-ay1)+max(0,bx2-bx1)*max(0,by2-by1);
-        return ar1-ar2;
+        int len1=abs(bx2-bx1);
+        int width1=abs(by1-by2);
+        int len2=abs(ax2-ax1);
+        int width2=abs(ay2-ay1);
+        int res=(len1*width1)+(len2*width2);
+        int maxix=max(ax1,bx1);
+        int maxiy=max(ay1,by1);
+        int minix=min(ax2,bx2);
+        int miniy=min(ay2,by2);
+       // int value=((maxix-minix) * abs(maxiy-miniy));
+       int x=minix-maxix;
+       int y=miniy-maxiy;
+       if(x<=0||y<=0)return res;
+        return res-(x*y);
     }
 };
