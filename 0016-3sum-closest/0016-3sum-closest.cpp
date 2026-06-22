f62a71d1d1976cec5bc96f28d93c34cebc5379a5
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-06-21 18:58:05 -0700

    Time: 15 ms (31.32%), Space: 14 MB (71.3%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int t) {
-       int n=nums.size();
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n=nums.size();
         sort(nums.begin(),nums.end());
         int mini=INT_MAX;
-        int s;
+        int ans;
         for(int i=0;i<n;i++){
-            if(i>0&&nums[i]==nums[i-1])continue;
-           int j=i+1;
-           int k=n-1;
-           while(j<k){
-             int total=nums[i]+nums[j]+nums[k];
-             if(total>t){
-                //mini=min(mini,abs(total-t));
-                if(mini>abs(total-t)){
-                    mini=abs(total-t);
-                  s=total;
+            int s=i+1;
+            int e=n-1;
+            int v=nums[i];
+            while(s<e){
+                //while(s+1<e && nums[s]==nums[s+1])s++;
+               // while(s+1<e && nums[e]==nums[e-1])e--;
+                int value=(nums[s]+nums[e]);
+                value+=v;
+                int diff=abs(value-target);
+                 //mini=min(mini,abs(value-target));
+                 if(diff<mini){
+                    mini=diff;
+                    ans=value;
+                 }
+                if(value==target)return value;
+                else if(value>target){
+                  // mini=min(mini,abs(value-target));
+                  e--;}
+                else{
+                  //  mini=min(mini,abs(value-target));
+                    s++;
                 }
-                k--;
-             }
-             else if(total<t){
-               // mini=min(mini,abs(total-t));
-               if(mini>abs(total-t)){
-                    mini=abs(total-t);
-                  s=total;
-                }
-                j++;
-             }
-             else{
-               /*ans.push_back({nums[i],nums[j],nums[k]});
-               j++;
-               while(nums[j]==nums[j-1]&&j<k)j++;
-               */
-               return total;
-             }
-           }
+            }
+            while(i+1<n && nums[i]==nums[i+1])i++;
         }
-        return s; 
+        return ans;
     }
 };
