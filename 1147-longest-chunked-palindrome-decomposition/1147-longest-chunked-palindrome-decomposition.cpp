59ea8ec544796ae65b4c04fecd05ff4d822c756c
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-07-15 02:07:58 -0700

    Time: 0 ms (100%), Space: 11 MB (40.64%) - LeetHub

--- 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-string s;
-int dp[1005][1005];
-int dfs(int i,int j){
-   if(i>=j)return 0;
-   int mid=(j-i)/2;
-   if(dp[i][j]!=-1)return dp[i][j];
-   cout<<mid<<endl;
-   for(int len=1;len<=mid;len++){
-    if(s.compare(i,len,s,j-len,len)==0){
-        return 2+dfs(i+len,j-len);
-    }
-   }
-   return dp[i][j]=1;
-   
-   
-
-}
-    int longestDecomposition(string text) {
-       s=text;
-      int n=s.size();
-      for(int i=0;i<1004;i++){
-        for(int j=0;j<1004;j++){
-            dp[i][j]=-1;
+    int longestDecomposition(string s) {
+        int n=s.size();
+        int ans=1;
+        deque<char>dq1,dq2;
+        for(int i=0;i<(n/2);i++){
+          char ch1=s[i];
+          char ch2=s[n-i-1];
+          dq1.push_back(ch1);
+          dq2.push_front(ch2);
+          if(dq1==dq2){
+            if(i+1==(n-i-1)){
+                ans--;
+            }
+            ans+=2;
+            dq1.clear();
+            dq2.clear();
+          }
         }
-      }
-     return dfs(0,n);
+        return ans;
     }
 };
