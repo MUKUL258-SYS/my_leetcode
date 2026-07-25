1f8ffe6560992f81183f1472581e6988b50d5080
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-07-24 18:31:12 -0700

    Time: 40 ms (25.5%), Space: 78.4 MB (15.66%) - LeetHub

--- 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.cpp
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
 int helper(int a,int  b){
-   int cnt = 1;              // the added edge itself
-    while(a != b){
-        if(a > b) a /= 2;     // move the deeper (larger-valued) node up
-        else b /= 2;
-        cnt++;
+ int cnt=1;
+ while(a!=b){
+    cnt++;
+    if(a>b){
+        //cnt++;
+        a/=2;
     }
-    return cnt;
+    else{
+    b/=2;
+    }
+ }
+ return cnt;
 }
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
        vector<int>res;
