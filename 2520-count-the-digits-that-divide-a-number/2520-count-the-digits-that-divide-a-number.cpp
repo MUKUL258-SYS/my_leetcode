630eed8b756ab7ddfe890d37fad9bf564a0d6403
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-07-25 22:43:48 -0700

    Time: 0 ms (100%), Space: 7.7 MB (74.72%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int n=num;
+        int cnt=0;
+        while(num>0){
+            int rem=num%10;
+            if(rem>0 && n%rem==0){
+                cnt++;
+            }
+            num/=10;
+        }
+        return cnt;
+    }
+};
