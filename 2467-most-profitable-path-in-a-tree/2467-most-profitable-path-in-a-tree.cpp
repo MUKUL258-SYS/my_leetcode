2b39eee1443f229ffc26666e275578e0aa7b795a
MUKUL258-SYS <129175955+MUKUL258-SYS@users.noreply.github.com>
2026-09-14 03:43:26 -0700

    Time: 302 ms (11.94%), Space: 195.7 MB (5.06%) - LeetHub

---
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+vector<vector<int>>adj;
+bool dfs(int src,int time ,unordered_map<int,int>&path,vector<bool>& vis){
+    vis[src]=true;
+    path[src]=time;
+    if(src==0)return true;
+    for(auto nei:adj[src]){
+        if(!vis[nei]){
+            if(dfs(nei,time+1,path,vis))return true;
+        }
+    }
+    path.erase(src);
+    return false;
+}
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
+    int n=edges.size();
+    adj.resize(n+5);
+    for(auto e:edges){
+        adj[e[0]].push_back(e[1]);
+        adj[e[1]].push_back(e[0]);
+    }
+    queue<vector<int>>q;
+    int ans=INT_MIN;
+    vector<bool>vis(n,false);
+    unordered_map<int,int>mp;
+    dfs(bob,0,mp,vis);
+    vis.assign(n,false);
+   // vis[0]=true;
+    q.push({0,0,0});
+    while(!q.empty()){
+        auto f=q.front();
+        int node=f[0];
+        int time=f[1];
+        int inc=f[2];
+        q.pop();
+        vis[node]=true;
+        if(mp.find(node)==mp.end()){
+            inc+=amount[node];
+        }
+        else{
+            if(time<mp[node]){
+                inc+=amount[node];
+            }
+            else if(time==mp[node]){
+                inc+=(amount[node]/2);
+            }
+        }
+        if(adj[node].size()==1 && node!=0)
+        ans=max(ans,inc);
+        
+        for(auto nei:adj[node]){
+            if(!vis[nei]){
+                q.push({nei,time+1,inc});
+            }
+        }
+    }
+    return ans;
+    
+        
+    }
+};
